bc12fd4b59131fd4d239071dc5696870f3c41fb8
鼎岳 <dingyue@taobao.com>
2017-07-07 10:05:04 +0800

    隐藏元组

--- array/merge_intervals.py
+++ array/merge_intervals.py
@@ -7,11 +7,13 @@ Given [1,3],[2,6],[8,10],[15,18],
 return [1,6],[8,10],[15,18].
 """
 
+
 # Definition for an interval.
 class Interval(object):
     def __init__(self, s=0, e=0):
         self.start = s
         self.end = e
+
 
 def merge(intervals):
     """
@@ -24,22 +26,22 @@ def merge(intervals):
         if out and i.start <= out[-1].end:
             out[-1].end = max(out[-1].end, i.end)
         else:
-            # 列表的 += 等于 append ??
+            # += 要求右侧的参数必须实现iteration，但是i是Interval 没有实现对应的接口 于是 使用 i, 来表示一个一元的元组
             out += i,
     return out
+
 
 def print_intervals(intervals):
     res = []
     for i in intervals:
-        res.append('['+str(i.start)+','+str(i.end)+']')
+        res.append('[' + str(i.start) + ',' + str(i.end) + ']')
     print("".join(res))
 
+
 if __name__ == "__main__":
-    given = [[1,3],[8,10],[2,6],[15,18]]
-    intervals = []
+    given = [[1, 3], [8, 10], [2, 6], [15, 18]]
     # 自动解包
-    for l, r in given:
-        # intervals.append(Interval(l,r))
-        intervals += (Interval(l,r)),
+    intervals = [Interval(l, r) for l, r in given]
+
     print_intervals(intervals)
     print_intervals(merge(intervals))
